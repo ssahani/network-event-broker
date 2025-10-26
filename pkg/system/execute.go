42e0bbebceaa2a41dab31f6466e494c6a42b2ea5
Susant Sahani <ssahani@gmail.com>
2025-10-26 07:06:54 +0530

    Refactor pkg/system/execute.go to use Zerolog and add script execution timeout

--- pkg/system/execute.go
+++ pkg/system/execute.go
@@ -1,54 +1,73 @@
-// Copyright 2023 VMware, Inc.
 // SPDX-License-Identifier: Apache-2.0
+// Copyright 2023 VMware, Inc.
 
 package system
 
 import (
+	"context"
+	"fmt"
 	"os"
 	"os/exec"
-	"path"
+	"path/filepath"
 	"strconv"
+	"time"
 
-	log "github.com/sirupsen/logrus"
+	"github.com/rs/zerolog/log"
 
 	"github.com/vmware/network-event-broker/pkg/conf"
 )
 
+// defaultScriptTimeout defines the maximum duration for script execution.
+const defaultScriptTimeout = 5 * time.Second
+
+// ExecuteScripts runs scripts in the routes-modified.d directory for a given link.
 func ExecuteScripts(link string, index int) error {
-	scripts, err := ReadAllScriptInConfDir(path.Join(conf.ConfPath, conf.RoutesModifiedDir))
+	if link == "" {
+		log.Error().Msg("Link name cannot be empty")
+		return fmt.Errorf("link name cannot be empty")
+	}
+	if index < 0 {
+		log.Error().Int("index", index).Msg("Invalid link index")
+		return fmt.Errorf("invalid link index: %d", index)
+	}
+
+	scriptDir := filepath.Join(conf.ConfPath, conf.RoutesModifiedDir)
+	scripts, err := ReadAllScriptInConfDir(scriptDir)
 	if err != nil {
-		log.Errorf("Failed to read script dir '%s'", path.Join(conf.ConfPath, conf.RoutesModifiedDir))
-		return err
+		log.Error().Str("dir", scriptDir).Err(err).Msg("Failed to read script directory")
+		return fmt.Errorf("failed to read script directory %s: %w", scriptDir, err)
 	}
 
-	if len(scripts) <= 0 {
-		log.Debugf("No script in '%+v'", conf.RoutesModifiedDir)
-		return err
+	if len(scripts) == 0 {
+		log.Debug().Str("dir", scriptDir).Msg("No scripts found in directory")
+		return nil
 	}
 
-	path.Join(conf.ConfPath, conf.RoutesModifiedDir)
-	linkNameEnvArg := "LINK=" + link
-	linkIndexEnvArg := "LINKINDEX=" + strconv.Itoa(index)
+	// Prepare environment variables.
+	env := append(os.Environ(),
+		"LINK="+link,
+		"LINKINDEX="+strconv.Itoa(index),
+	)
 
-	for _, s := range scripts {
-		script := path.Join(conf.ConfPath, conf.RoutesModifiedDir, s)
+	for _, script := range scripts {
+		scriptPath := filepath.Join(scriptDir, script)
+		log.Debug().Str("script", scriptPath).Str("link", link).Int("index", index).Msg("Executing script")
 
-		log.Debugf("Executing script '%s' in dir='%v' for link='%s'", script, conf.RoutesModifiedDir, link)
+		// Use context to enforce timeout.
+		ctx, cancel := context.WithTimeout(context.Background(), defaultScriptTimeout)
+		defer cancel()
 
-		cmd := exec.Command(script)
-		cmd.Env = append(os.Environ(),
-			linkNameEnvArg,
-			linkNameEnvArg,
-			linkIndexEnvArg,
-		)
+		cmd := exec.CommandContext(ctx, scriptPath)
+		cmd.Env = env
 
 		if err := cmd.Run(); err != nil {
-			log.Errorf("Failed to execute script='%s': %v", script, err)
+			log.Error().Str("script", scriptPath).Str("link", link).Int("index", index).Err(err).Msg("Failed to execute script")
 			continue
 		}
 
-		log.Debugf("Successfully executed script '%s' in dir='%v' for link='%s'", script, conf.RoutesModifiedDir, link)
+		log.Debug().Str("script", scriptPath).Str("link", link).Int("index", index).Msg("Successfully executed script")
 	}
 
+	log.Debug().Str("dir", scriptDir).Int("count", len(scripts)).Str("link", link).Int("index", index).Msg("Completed script execution")
 	return nil
 }
